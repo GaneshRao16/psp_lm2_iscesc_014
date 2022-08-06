6e10ce1d441bdc0160fa4f76bcacd451d6b51917
GaneshRao16 <110719353+GaneshRao16@users.noreply.github.com>
2022-08-06 16:29:26 +0530

    adding number is commited

---
+++ add.c
@@ -0,0 +1,11 @@
+// Adding 2 numbers
+#include<stdio.h>
+int main()
+{
+ int a,b;
+  printf("Enter 2 numbers a & b to be added \n");
+  scanf("%d%d",&a,&b);
+  printf("sum=%d+%d",a,b);
+  return 0;
+  
+}
