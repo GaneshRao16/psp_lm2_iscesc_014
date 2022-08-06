ca3d793c3545d11c42002160c1275bc5015faf78
GaneshRao16 <110719353+GaneshRao16@users.noreply.github.com>
2022-08-06 16:45:22 +0530

    committed adding

---
+++ add1.c
@@ -0,0 +1,15 @@
+// Add 2 numbers using different logic
+#include<stdio.h>
+int main()
+{
+  int a,b,sum;
+  printf("Enter 2 numbers to be added\n");
+  printf("a=");
+  scanf("%d",&a);
+  printf("b=");
+  scanf("%d",&b);
+  sum=a+b;
+  printf("sum=%d+%d=%d",a,b,sum);
+  return 0;
+  
+}
